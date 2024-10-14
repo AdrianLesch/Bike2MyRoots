3f9799e5248c5f6aba9c50a837620e1dae4525d4
Adrian Lesch <al.go.rhythmus@gmx.de>
2024-10-14 20:21:08 +0200

    1st implementation of async newsbox done

--- script.js
+++ script.js
@@ -1,21 +1,18 @@
 //This is the main-script for some of the special functions like asyncronous operations
-
-console.log("Hello");
-
 async function fetchNews() {
     try {
         let jsonData = await fetch("Mockdata.json");
         let data = await jsonData.json();
 
         displayData(data);
-        console.log(data);
     } catch (error) {
         console.error("Error fetching data:", error);
     }
 }
 
 function displayData(data) {
-    document.getElementById("newsbox").innerText = data.text;
+    const newsbox = document.getElementById("newsbox");
+    newsbox.innerText = `${data.newMessage.message}\n${data.newMessageTwo.message}`;
 }
 
 fetchNews();
